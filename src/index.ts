2a0f1da72ea4bf0babd3c304ab29ab260d2cbd5d
teppeis <teppeis@gmail.com>
2019-11-19 11:48:52 +0900

    style: reformat with latest eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export {ArrayMultimap} from './arraymultimap';
-export {SetMultimap} from './setmultimap';
+export { ArrayMultimap } from "./arraymultimap";
+export { SetMultimap } from "./setmultimap";
