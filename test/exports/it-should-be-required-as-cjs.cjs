600a2a905076c05ce7e99a87080e930dc42f5638
teppeis <teppeis@gmail.com>
2022-09-19 17:47:26 +0900

    test: improve exports test

--- test/exports/it-should-be-required-as-cjs.cjs
+++ test/exports/it-should-be-required-as-cjs.cjs
@@ -1,7 +1,7 @@
 "use strict";
 
 const { ArrayMultimap } = require("@teppeis/multimaps");
-const assert = require("assert");
+const { strict: assert } = require("assert");
 
 const map = new ArrayMultimap();
-assert.strictEqual(map.size, 0);
+assert.equal(map.size, 0);
